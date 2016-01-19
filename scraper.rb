930c0f81cc1f072d6a029b795db8e34aabeaaf63
Tony Bowden <tony@tmtm.com>
2016-01-19 07:37:29 +0000

    Also scrape Arabic version

--- scraper.rb
+++ scraper.rb
@@ -4,6 +4,14 @@
 require 'pry'
 require 'wikidata/fetcher'
 
+
+column_header = 'النائب'
+ar_names = EveryPolitician::Wikidata.wikipedia_xpath( 
+  url: 'https://ar.wikipedia.org/wiki/مجلس_النواب_اللبناني',
+  after: '//h2[contains(.,"2009")]',
+  xpath: '//table[.//td[contains(.,"%s")]]//td[2]//a[not(@class="new")]/@title' % column_header,
+)
 en_names = EveryPolitician::Wikidata.morph_wikinames(source: 'tmtmtmtm/lebanese-parliament-2009', column: 'wikiname')
-EveryPolitician::Wikidata.scrape_wikidata(names: { en: en_names }, output: false)
+
+EveryPolitician::Wikidata.scrape_wikidata(names: { en: en_names, ar: ar_names }, output: false)
 warn EveryPolitician::Wikidata.notify_rebuilder
